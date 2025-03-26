bca9aa2aa3984a1f1d32787afa9615697c34c863
CRISTIAN OLIVEIRA COSTA <cristianoliveira2145@gmail.com>
2025-03-26 18:33:20 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,76 @@
+# nome = input('Digite seu nome: ')
+# senha = input('Digite sua senha: ')
+
+# if nome == 'admin' and senha == '1234':
+#     print('Seja Bem-vindo ao meu sitema! ')
+# else:
+#     print('nome ou senha incorreta!')
+
+# peso = float(input('Digite seu peso: '))
+# altura = float(input('Digite sua altura: '))
+
+# imc = peso /(altura ** 2)
+
+# if imc <= 18.50:
+#     print(f'Você esta abaixo do peso {imc:.2f}')
+# elif 18.5< imc< 25:
+#     print(f'Você esta com peso normal {imc:.2f}')
+# elif 25< imc < 29.99:
+#     print(f'Você esta com sobrepeso {imc:.2f}')
+# elif imc> 30:
+#     print(f'Você esta com Obesidade {imc:.2f}')
+
+# numero = 1
+# while numero < 10:
+#     print(numero)
+#     numero += 1 
+
+# senha = ''
+# while senha != "1234":
+#     senha=input("digite sua senha: ")
+# print("senha correta!")
+
+# numero = 1
+# while numero < 11:
+#     print(numero)
+#     numero += 1 
+
+# numero =  1 
+# soma = 0
+# while numero < 101:
+#     soma += numero
+#     print(soma)
+#     numero += 1
+
+# numero = int(input('Digite numero:'))
+# soma = 0
+# while soma <= 10:
+#     resultado = numero * soma 
+#     print(f'{numero}x{soma} = {resultado}')
+#     soma += 1
+
+# while True:
+#     print('''
+#         1 - Pedir a idade do usuário
+#         2 - Pedir o nome do usuário
+#         3 - Sair
+#     ''')
+
+#     opcao = int(input('Escolha a opção: '))
+
+#     if opcao == 1:
+#         idade = int(input('Digite a sua idade: '))
+#     elif opcao == 2:
+#         nome = input('Digite seu nome: ')
+#     elif opcao == 3:
+#         break
+
+# numero = int(input('escolha um número: '))
+
+# contador = 1
+# while contador <= numero:
+#     if contador % 2 == 0:
+#         contador += 1
+#     else:
+#         print(contador)
+#         contador += 1
