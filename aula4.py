dbc9ccc035f29da73007a3acef23c29d119be4f2
Cristian-O-Costa <cristianoliveira2145@gmail.com>
2025-04-02 18:32:06 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,108 @@
+# nome = 'Felipe'
+
+# while nome == 'Felipe':
+#     nome = input('Digite seu nome: ')
+#     if nome == 'João':
+#         break
+
+# nome = 'Felipe'
+
+# while nome == 'Felipe':
+#     nome = input('Digite seu nome: ')
+#     if nome == 'João':
+#         break
+
+# nome = 'Felipe'
+
+# for i in nome:
+#     print(i)
+
+# numero = 2
+# while numero < 10:
+#     print(numero)
+#     numero += 2
+
+# print('##########################')
+# numero = int(input('Digite um número: '))
+# numero_2 = int(input('Digite um número: '))
+
+# for i in range(numero_2, numero):
+#     print(i)
+
+
+# Atividade 1
+# num = int(input('Digite um número: '))
+
+# for i in range(1, 11):
+#     print(f'{num} x {i} = {num*i}')
+
+# Atividade 2
+# soma = 0
+# for i in range(1, 101):
+#     print(f'{soma} + {i} = {i + soma}')
+#     soma += i
+
+# print(soma)
+
+# palavra = input('Digite uma palavra: ')
+# # print(palavra[0])
+# # print(palavra[1])
+# # print(palavra[2])
+# # print(palavra[3])
+# # print(palavra[4])
+
+# for letra in palavra:
+#     print(letra)
+
+
+# for i in range(10, 0, -1):
+#     print(i) 
+
+# print("feliz Ano Novo")
+
+# positivo = 0
+# negativo = 0  
+
+# for i in range(10):
+#     numero = int(input('Digite Seu numero: '))
+
+#     if numero > 0:
+#         positivo += 1
+#     elif numero < 0:
+#         negativo += 1
+
+# print(f"Quantidade de numeros positivos: {positivo}")
+# print(f"Quantidade de numeros negativos: {negativo}")
+
+
+# soma = 0
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         soma += i
+    
+# print(soma)
+
+
+# palavra = input('Ditige tua palavra: ').lower()
+# qtd_vogais = 0 
+
+# for i in palavra:
+#     if i in 'aeiou':
+#         qtd_vogais += 1
+
+# print(qtd_vogais)
+
+# soma = 0
+# for i in range(5):
+#     nota = float(input('digite sua nota: '))
+#     soma += nota 
+
+# media = soma / 5
+
+# if media >= 6:
+#     print('Você foi aprovado!')
+# else:
+#     print('você se fudeu! mais um ano ótario ')
+
+
+
