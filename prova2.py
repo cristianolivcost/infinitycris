073e4b4ec951722bf5e1b9f26e83dcdca72ea459
Cristian-O-Costa <cristianoliveira2145@gmail.com>
2025-05-15 19:57:03 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,10 @@
+lista_de_incrição = {}
+
+lista_de_incrição["nome"] = input("Digite seu nome: ")
+lista_de_incrição["telefone"] = input("Digite seu telefone: ")
+lista_de_incrição["email"] = input("Digite seu email: ")
+
+print("\ninformações de inscrição: ")
+print("Nome:", lista_de_incrição["nome"])
+print("Telefone:", lista_de_incrição["telefone"])
+print("Email:", lista_de_incrição["email"])
